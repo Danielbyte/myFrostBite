f6bc64b165f3c69708e660e1618317b53bee0385
Danielbyte <danielmasango7@gmail.com>
2023-02-24 20:45:22 +0200

    Initialize fish

--- game-source-code/fish.cpp
+++ game-source-code/fish.cpp
@@ -1,3 +1,32 @@
 #include "fish.h"
 
-Fish::Fish() {}
+Fish::Fish(const int& side, const float& region)
+{
+	pos.y = region;
+	if (side == right)
+	{
+		pos.x = windowWidth - (fish_width / 2.0f);
+	}
+	else if (side == left)
+	{
+		pos.x = fish_width / 2.0f;
+	}
+}
+
+vector2f Fish::get_position() const
+{
+	return pos;
+}
+
+void Fish::set_x_position(const float& _x)
+{
+	if (pos.x > (fish_width / 2.0f))
+	{
+		pos.x -= _x;
+	}
+
+	else
+	{
+		pos.x += _x;
+	}
+}
--- game-source-code/fish.h
+++ game-source-code/fish.h
@@ -1,12 +1,18 @@
 #ifndef FISH_H
 #define FISH_H
 
+#include "gameConstants.h"
+#include "SfmlLibrary.h"
+
 class Fish
 {
 public:
-	Fish();
+	Fish(){}
+	Fish(const int&, const float&);
+	vector2f get_position() const;
+	void set_x_position(const float&);
 private:
-
+	vector2f pos;
 };
 #endif
 
