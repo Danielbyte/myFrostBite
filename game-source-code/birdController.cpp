5534febd1922c715ac18c474855a52d0595ced36
Danielbyte <danielmasango7@gmail.com>
2023-02-26 02:04:22 +0200

    initialize class to update bird movement

---
+++ game-source-code/birdController.cpp
@@ -0,0 +1,3 @@
+#include "birdController.h"
+
+BirdController::BirdController(){}
---
+++ game-source-code/birdController.h
@@ -0,0 +1,13 @@
+#ifndef BIRDCONTROLLER_H
+#define BIRDCONTROLLER_H
+
+#include "bird.h"
+
+class BirdController
+{
+public:
+	BirdController();
+
+private:
+};
+#endif;
