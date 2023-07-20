ae8f2f8038ab124e52246d41cc1910840687d324
Danielbyte <danielmasango7@gmail.com>
2023-07-20 17:22:09 +0200

    Initialized point system

--- game-source-code/Header-files/ScoreManager.h
+++ game-source-code/Header-files/ScoreManager.h
@@ -13,4 +13,8 @@ public:
 	void updatePlayerScore(shared_ptr<Player>&, const std::string _coreType);
 private:
 	float high_score;
+	int iceBlockPts; //points per ice block
+	int winning; // points for winning
+	int enteringIgloo; //points for entering igloo
+	int fish; //points for eating fish
 };
--- game-source-code/Source-files/ScoreManager.cpp
+++ game-source-code/Source-files/ScoreManager.cpp
@@ -1,7 +1,11 @@
 #include "../Header-files/ScoreManager.h"
 
 ScoreManager::ScoreManager():
-	high_score{0.0f}
+	high_score{0.0f},
+	iceBlockPts{30},
+	winning{0},
+	enteringIgloo{480},
+	fish{30}
 {}
 
 float ScoreManager::getHighScore()
